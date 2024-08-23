cb06c3216f19891ec8b27d03faf49fc00c03cfd7
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-08-23 11:50:26 +0300

    initial commit

---
+++ src/test/java/me/jazzy/e_commerce_app/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jazzy.e_commerce_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
